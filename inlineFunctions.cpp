c74a2169e6fcd39a10c22c2440d528cc41897eab
ibraheemchand <chibraheemchand@gmail.com>
2026-02-16 08:42:05 +0500

    OOP lab

---
+++ inlineFunctions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+inline int square(int a){
+        return a*a;
+    }
+
+int main(){
+
+    int a = 2;
+   cout << square(a) <<endl ;
+    return 0 ;
+}
