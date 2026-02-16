5c4df63239b3693539497f6d6c6060ebea8985e6
ibraheemchand <chibraheemchand@gmail.com>
2026-02-16 09:05:54 +0500

    Highest gpa Class arry

--- code4.cpp
+++ code4.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 using namespace std;
 
+class students
+{
+public:
+string name;
+int rollNo;
+float gpa;  
+    
+};
+
 
 
 int main(){
+students abc[5];
+for (int i = 0; i < 5; i++)
+{
+    cout << "Enter Name "<< endl;
+    cin >> abc[i].name;
 
+     cout << "Enter Roll no. "<< endl;
+    cin >> abc[i].rollNo;
+
+     cout << "Enter gpa"<< endl;
+    cin >> abc[i].gpa;
+}
+
+float highestM = 0 ;
+int n=0;
+ for (int i = 0; i < 5; i++)
+ {
+    
+    if (abc[i].gpa > highestM)
+    {
+       highestM = abc[i].gpa ;
+        n=i;
+    }
+    
+}
+ 
+cout << "The highest gpa is " << abc[n].gpa << endl ;
    
     return 0 ;
 }
