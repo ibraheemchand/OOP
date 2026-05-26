7bda4bb69890599ae217b5542d96a162a2048b23
ibraheemchand <chibraheemchand@gmail.com>
2026-05-26 21:00:22 +0500

    Learning java

---
+++ java/helo.java
@@ -0,0 +1,16 @@
+public class helo {
+
+    public static void main(String[] args) {
+        int[] marks = new int[4];
+
+      for (int i = 0; i < 4; i++) {
+         marks[i]=3;
+      }
+        for (int i = 0; i < 4; i++) {
+            System.out.println("The number is "+marks[i]);
+        }
+    
+    System.out.println(marks);
+    }
+
+}
